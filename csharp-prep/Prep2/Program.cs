375f6507c20bc2b8c053e5cf9bda8222442f4b35
rag5798 <goe23002@byui.edu>
2023-09-16 23:41:07 -0600

    if else for percent to letter grade

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your percentage grade: ");
+        int grade = Console.Read();
+        string letter;
+
+        if (grade>=90){
+            letter = "A";
+            Console.WriteLine($"You got an {letter}");
+        }else if(grade>=80){
+            letter = "B";
+            Console.WriteLine($"You got an {letter}");
+        }else if(grade>=70){
+            letter = "C";
+            Console.WriteLine($"You got an {letter}");
+        }else if(grade>=60){
+            letter = "D";
+            Console.WriteLine($"You got an {letter}");
+        }else if(grade<60){
+            letter = "F";
+            Console.WriteLine($"You got an {letter}");
+        }else{
+            Console.WriteLine("Error");
+        }
     }
 }
