53b0e79c5be2104fef3fb076eb2cb400fc60ed0e
rag5798 <goe23002@byui.edu>
2023-09-16 22:31:52 -0600

    added lastname, first last

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -8,7 +8,7 @@ class Program
         string firstname = Console.ReadLine();
         Console.Write("What is your last name? ");
         string lastname = Console.ReadLine();
-        Console.WriteLine($"Your name is, {firstname} {lastname}.");
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
 
         
     }
