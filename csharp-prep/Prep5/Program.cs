beced956eab49c5df813796eb884678f72bdc588
rag5798 <goe23002@byui.edu>
2023-09-20 13:59:49 -0600

    made functions for Prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -3,31 +3,43 @@ using System.ComponentModel;
 
 class Program
 {
-    public string DisplayWelcome(){
+    string DisplayWelcome(){
         string welcome = "Welcome to the program!";
         return welcome;
     }
 
-    public string PromptUserName(){
+    string PromptUserName(){
         Console.Write("Please enter your name: ");
         string name = Console.ReadLine();
         return name;
     }
 
-    public int PromptUserNumber(){
+    int PromptUserNumber(){
         Console.Write("Please enter your favorite number: ");
         int num = int.Parse(Console.ReadLine());
         return num;
     }
 
-    public int SquareNumber(int num){
-        int SquareNumber = 0;
-        return SquareNumber;
+    int SquareNumber(int num){
+        int square = num * num;
+        return square;
     }
+
+    void DisplayResult(string name, int square){
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
+
+
 
 
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Program pro = new Program();
+        pro.DisplayWelcome();
+        string name = pro.PromptUserName();
+        int num = pro.PromptUserNumber();
+        num = pro.SquareNumber(num);
+        pro.DisplayResult(name, num);
+
     }
 }
