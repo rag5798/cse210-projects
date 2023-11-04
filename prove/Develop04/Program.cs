bed79bf226ae991be8ce0afbae36a484d086dee4
rag5798 <goe23002@byui.edu>
2023-11-03 19:13:27 -0600

    changed menu

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -11,20 +11,20 @@ class Program
         Listening listen = new Listening();
         bool cont = true;
         while (cont != false){
-            Console.WriteLine("Menu Options:\n1.) Start Breathing Activity\n2.) Start Reflection Activity\n3.) Start Listing Activity\n4.) Listening Activity\n5.)Quit");
+            Console.WriteLine("Menu Options:\n1.) Start Breathing Activity\n2.) Start Reflection Activity\n3.) Start Listing Activity\n4.) Start Listening Activity\n5.)Quit");
             string answer = Console.ReadLine();
             int choice;
             bool ans = int.TryParse(answer, out choice);
             while (ans != true){
                 Console.WriteLine("\n\n***Please Enter an Integer 1, 2, or 3***\n\n");
-                Console.WriteLine("Menu Options:\n1.) Start Breathing Activity\n2.) Start Reflection Activity\n3.) Start Listing Activity\n4.) Listening Activity\n5.)Quit");
+                Console.WriteLine("Menu Options:\n1.) Start Breathing Activity\n2.) Start Reflection Activity\n3.) Start Listing Activity\n4.) Start Listening Activity\n5.)Quit");
                 answer = Console.ReadLine();
                 ans = int.TryParse(answer, out choice);
             }
 
             if (choice < 1 || choice > 5){
                 Console.WriteLine("\n\n***Please Enter an Integer 1, 2, or 3***\n\n");
-                Console.WriteLine("Menu Options:\n1.) Start Breathing Activity\n2.) Start Reflection Activity\n3.) Start Listing Activity\n4.) Listening Activity\n5.)Quit");
+                Console.WriteLine("Menu Options:\n1.) Start Breathing Activity\n2.) Start Reflection Activity\n3.) Start Listing Activity\n4.) Start Listening Activity\n5.)Quit");
                 answer = Console.ReadLine();
                 ans = int.TryParse(answer, out choice);
             }
