312bf55080cf99eb60883c6664f192597aa98113
rag5798 <goe23002@byui.edu>
2023-12-08 03:11:32 -0700

    User creation fully implemented

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,6 +4,108 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        while (true)
+        {
+            Console.WriteLine("WELCOME TO THE LIBRARY");
+            Console.WriteLine("Please Select an option from the menu using the associated number:");
+            Console.WriteLine("1.) Login\n2.) Create User");
+            int num;
+            bool test = int.TryParse(Console.ReadLine(), out num);
+            while (test == false || num > 2 || num < 0)
+            {
+                Console.WriteLine("Please Select an option from the menu using the associated number:");
+                Console.WriteLine("1.) Login\n2.) Create User");
+                test = int.TryParse(Console.ReadLine(), out num);
+            }
+
+            if (num == 1)
+            {
+                string filepath = "Users.txt";
+                bool exists = File.Exists(filepath);
+                if (exists == false)
+                {
+                    Console.WriteLine("Please Create a User Before Login");
+                    continue;
+                }
+                bool isempty = new FileInfo(filepath).Length == 0;
+                if (isempty == true)
+                {
+                    Console.WriteLine("Please Create a User Before Login");
+                    continue;
+                }
+
+                User u = new User();
+                Console.Write("Please input a username:");
+                string user = Console.ReadLine();
+                u.SetUserName(user);
+
+                int pin;
+                test = int.TryParse(Console.ReadLine(), out pin);
+                while (test == false || pin > 9999 || pin < 1000)
+                {
+                    Console.Write("Please input a 4 digit pin:");
+                    test = int.TryParse(Console.ReadLine(), out pin);
+                }
+                u.SetPin(pin);
+                bool checkuser = u.CheckUserName();
+                bool checkpin = u.CheckPin();
+                while (checkuser == false || checkpin == false)
+                {
+                    Console.Write("Please input a username:");
+                    user = Console.ReadLine();
+                    u.SetUserName(user);
+
+                    test = int.TryParse(Console.ReadLine(), out pin);
+                    while (test == false || pin > 9999 || pin < 1000)
+                    {
+                        Console.Write("Please input a 4 digit pin:");
+                        test = int.TryParse(Console.ReadLine(), out pin);
+                    }
+                    u.SetPin(pin);
+                    checkuser = u.CheckUserName();
+                    checkpin = u.CheckPin();
+                }
+                
+
+            }else if (num == 2)
+            {
+                string filepath = "Users.txt";
+                bool fileexists = File.Exists(filepath);
+                User u = new User();
+                Console.Write("Please input a username:");
+                string user = Console.ReadLine();
+                u.SetUserName(user);
+                if (fileexists == true)
+                {
+                    bool exists = u.CheckUserName();
+                    while (exists == true)
+                    {
+                        Console.Write("Please input a username:");
+                        user = Console.ReadLine();
+                        u.SetUserName(user);
+                        exists = u.CheckUserName();
+                    }
+                }
+                
+
+
+                Console.Write("Please input a 4 digit pin:");
+                int pin;
+                test = int.TryParse(Console.ReadLine(), out pin);
+                while (test == false || pin > 9999 || pin < 1000)
+                {
+                    Console.Write("Please input a 4 digit pin:");
+                    test = int.TryParse(Console.ReadLine(), out pin);
+                }
+
+                u.SetPin(pin);
+                u.SaveUserToFile();
+
+            }else
+            {
+                Console.WriteLine("An Error Has Occured");
+            }
+            
+        }
     }
 }
--- final/FinalProject/User.cs
+++ final/FinalProject/User.cs
@@ -1,7 +1,130 @@
-class User{
+using System.ComponentModel.Design;
+
+class User
+{
     private string username;
 
     private int pin;
 
-    
+    public User(string u, int p)
+    {
+        username = u;
+        pin = p;
+    }
+
+    public User(string u){
+        username = u;
+        pin = 1111;
+    }
+
+    public User()
+    {
+        username = "";
+        pin = 1111;
+    }
+
+    public void SetUserName(string u)
+    {
+        username = u;
+    }
+
+    public string GetUserName()
+    {
+        return username;
+    }
+
+    public void SetPin(int p)
+    {
+        pin = p;
+    }
+
+    public int GetPin()
+    {
+        return pin;
+    }
+
+    public bool CheckUserName()
+    {
+        string filePath = "Users.txt";
+        bool exists = false;
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line.Contains(username))
+                {
+                    exists = true;
+                    break;
+                }
+            }
+        }
+        if (exists)
+        {
+            //Console.WriteLine("The username exists. Please Choose a different Username.");
+            return true;
+        }
+        else
+        {
+            //Console.WriteLine("User Created");
+            return false;
+        }
+
+
+    }
+
+    public bool CheckPin()
+    {
+        string filePath = "Users.txt";
+        bool exists = false;
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line.Contains(pin.ToString()))
+                {
+                    exists = true;
+                    break;
+                }
+            }
+        }
+        if (exists)
+        {
+            //Console.WriteLine("The username exists. Please Choose a different Username.");
+            return true;
+        }
+        else
+        {
+            //Console.WriteLine("User Created");
+            return false;
+        }
+    }
+
+    public void SaveUserToFile()
+    {
+        string filePath = "Users.txt";
+        string line = $"{username},{pin}";
+        File.AppendAllText(filePath, line + Environment.NewLine);
+
+    }
+
+    public void RemoveUser()
+    {
+        string filePath = "Users.txt";
+        string search = username;
+        string[] lines = File.ReadAllLines(filePath);
+        List<string> updatedLines = new List<string>();
+        foreach (string line in lines)
+        
+        {
+            if (!line.Contains(search))
+            
+            {
+                updatedLines.Add(line);
+            }
+        }
+        File.WriteAllLines(filePath, updatedLines.ToArray());
+
+    }
 }
